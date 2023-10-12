42fab27c536a719e5bf6cc159367aa2d935c76d7
meija1 <>
2023-10-12 14:44:53 +0000

    Add regression evaluation code

---
+++ src/machine_learning/regression_evaluation.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+def regression_performance(X_train, y_train, X_test, y_test, pipeline):
+    st.info("Train Set")
+    confusion_matrix_and_report(X_train, y_train, pipeline)
+
+    st.info("Test Set")
+    confusion_matrix_and_report(X_test, y_test, pipeline)
+
+
+def regression_report(X, y, pipeline):
+
+    prediction = pipeline.predict(X)
+
+    st.write('#### Mean Squared Error')
+    st.code(mean_squared_error(y, prediction))
+
+    st.write('#### Mean Absolute Error')
+    st.code(mean_absolute_error(y, prediction))
+
+    st.write('#### R2 Score')
+    st.code(r2_score(y, prediction))
+
+def regression_plot(X_train, y_train, prediction):
+    
+    prediction = pipeline.predict(X_train)
+
+    st.code(sns.scatterplot(x=prediction, y=y_train, alpha=0.5))
